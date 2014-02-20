0c4ee613c9e901e5e7ea4f0636d46f01aa2127dc
nobu42 <bastien_achard_42@hotmail.com>
2014-02-20 18:05:59 +0100

    ?

--- src/housing/models.py
+++ src/housing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class House(models.Model):
+    name = models.CharField(max_length = 30, verbose_name="Name")
+    surface = models.PositiveSmallIntegerField(verbose_name="Surface")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Price")
+
+    def __unicode__(self):
+        """ 
+        
+        """
+        return u"%s"%self.name
