e3309362bcf8427209e8e2c156c73c5e60b1ae6e
nobu42 <bastien_achard_42@hotmail.com>
2014-03-01 13:08:10 +0100

    change way to add photos & contributors

--- src/housing/models.py
+++ src/housing/models.py
@@ -13,9 +13,9 @@ class House(models.Model):
 
     
 class Photo(models.Model):
-    house = models.ForeignKey(House, editable=False)
-    img = models.ImageField(upload_to='housing')
-    descr = models.CharField(max_length=30, verbose_name="Description")
+    house = models.ForeignKey(House)
+    img = models.ImageField(upload_to='housing', null=True, blank=True)
+    descr = models.CharField(max_length=30, verbose_name="Description", null=True, blank=True)
     
     def __unicode__(self):
         return u"%s"%self.descr
@@ -40,7 +40,7 @@ class GPSCoordinate(models.Model):
 
 
 class Furniture(models.Model):
-    house = models.OneToOneField(House, editable=False)
+    house = models.OneToOneField(House)
     oven = models.BooleanField(verbose_name="Oven", default=False)
     fridge = models.BooleanField(verbose_name="Fridge", default=False)
     
