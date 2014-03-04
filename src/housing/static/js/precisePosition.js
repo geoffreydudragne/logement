e6cedc7972821eb8078de892bc9d7a7d283b528a
geoffrey <geoffreydudragne@hotmail.fr>
2014-03-04 16:17:22 +0100

    finished a very cool page for precising the position of your house

--- src/housing/static/js/precisePosition.js
+++ src/housing/static/js/precisePosition.js
@@ -1,12 +1,24 @@
 var map = $('#map_canvas').gmap('get', 'map');
+var geocoder = new google.maps.Geocoder();
 
-google.maps.event.addListener(map, "rightclick", function(event) {
+google.maps.event.addListener(map, "click", function(event) {
     var lat = event.latLng.lat();
     var lng = event.latLng.lng();
     $('#latitude').val(lat);
     $('#longitude').val(lng);
+    $('#map_canvas').gmap('clear', 'markers');
+    $('#map_canvas').gmap('addMarker', { 
+        'position': new google.maps.LatLng(lat, lng)
+    });
 });
 
-$("centerButton").click(function(){
-
+$("#centerButton").click(function(){
+    geocoder.geocode( { 'address': $('#address').val()}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            $('#map_canvas').gmap('option', 'zoom', 19);
+            $('#map_canvas').gmap('option', 'mapTypeId', google.maps.MapTypeId.SATELLITE);
+            $('#map_canvas').gmap('addMarker', { 'position': results[0].geometry.location });
+        }
+    });
 });
